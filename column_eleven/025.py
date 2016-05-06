5ec8789ceb9d72ff31cebb3c4fc76ec4201e6221
wuxiaoshen <wuxiaoshen@shu.edu.cn>
2016-05-06 10:35:50 +0800

    Eleven

---
+++ column_eleven/025.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
+
+# doc = {
+#     'author': 'kimchy',
+#     'text': 'Elasticsearch: cool. bonsai cool.',
+#     'timestamp': datetime.now(),
+# }
+# res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
+# print(res['created'])
+res = es.search(index="test-index", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
